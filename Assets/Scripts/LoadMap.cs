5e88295acf89c7e962fdef410cc783511001c536
Pippopad <pippopad2017@gmail.com>
2020-09-02 14:03:57 +0200

    Fixed Animation & Changed Some Textures

--- Assets/Scripts/LoadMap.cs
+++ Assets/Scripts/LoadMap.cs
@@ -5,10 +5,23 @@ public class LoadMap : MonoBehaviour
 {
     public string levelName;
 
+    public GameObject[] onTriggerActivate;
+    public GameObject[] onTriggerDeactivate;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if(onTriggerActivate.Length != 0)
+        {
+            for (int i = 0; i < onTriggerActivate.Length; i++)
+            {
+                Debug.Log("OKKO");
+                if (onTriggerActivate[i].activeSelf == false) onTriggerActivate[i].SetActive(true);
+            }
+        }
+
         if(collision.tag == "Player")
         {
+            Debug.Log("Teleport");
             SceneManager.LoadScene(levelName);
         }
     }
